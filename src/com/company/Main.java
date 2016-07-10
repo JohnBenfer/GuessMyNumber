5bce6f8f3cc19942c0086d8dee88e23fee9bf418
John Benfer <Jbenfer@ksu.edu>
2016-07-10 16:09:37 -0500

    added playGame method still can't repeat play with user input y/n

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,20 +8,18 @@ public class Main {
         System.out.println("Guess My Number");
         Scanner in = new Scanner(System.in);
         String repeat = "y";
-        while (repeat == "y") {
-            int max = getMax(in);
-            int num = getNumber(max);
-            int userGuess = getGuess(in);
-            String out = testInput(num, userGuess);
-            while (out != "Correct!") {
 
-                System.out.println(out);
-                userGuess = getGuess(in);
-                out = testInput(num, userGuess);
+        while (repeat == "y") {
+            System.out.println(repeat);
+            repeat = playGame(in, repeat);
+            System.out.println(repeat);
+            if (repeat == "n") {
+                repeat = "n";
+            } else if (repeat == "y") {
+                repeat = "y";
             }
-            System.out.println(out);
-            repeat = getRepeat(in);
         }
+
         System.out.println("Thanks for playing!");
 
         // debug System.out.println(userGuess);
@@ -30,11 +28,30 @@ public class Main {
 
 
     }
+    private static String playGame(Scanner in, String repeat) {
+        int max = getMax(in);
+        int num = getNumber(max);
+        int userGuess = getGuess(in);
+        String out = testInput(num, userGuess);
+        while (out != "Correct!") {
+
+            System.out.println(out);
+            userGuess = getGuess(in);
+            out = testInput(num, userGuess);
+        }
+        System.out.println(out);
+        repeat = getRepeat(in);
+
+        System.out.println(repeat);
+        return repeat;
+    }
+
 
     public static int getNumber(int max) {
         int randomNumber = (int)(Math.random() * max + 1);
         return randomNumber;
     }
+
 
     public static String testInput(int num, int userGuess) {
         String out;
@@ -51,8 +68,8 @@ public class Main {
             return out;
         }
         return null;
-
     }
+
 
     public static int getGuess(Scanner in) {
 
@@ -60,17 +77,19 @@ public class Main {
         return userGuess;
     }
 
+
     public static int getMax(Scanner in) {
         System.out.println("What would you like the max integer to be?");
         int max = in.nextInt();
         System.out.println("Okay, guess an integer between 1 and " + max + "! Good luck!");
         return max;
     }
+
+
     public static String getRepeat(Scanner in) {
         System.out.println("Congrats on the correct answer!");
         System.out.println("Would you like to play again? (y/n)");
         String repeat = in.next();
-        System.out.println(repeat);
         return repeat;
     }
 }
